bb1e007bc937338f70e7ba109274347276fff351
= <=>
2022-01-27 10:27:08 +0200

    removed config and node modules

--- functions/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports.firebaseConfig = {
-  apiKey: 'AIzaSyDCoTUiQTLyKTIlNZAO64FqCZuGXzsAyAM',
-  authDomain: 'squeaker-app.firebaseapp.com',
-  projectId: 'squeaker-app',
-  storageBucket: 'squeaker-app.appspot.com',
-  messagingSenderId: '402839695833',
-  appId: '1:402839695833:web:4335e72d9e1a4b1c8c69c8',
-  measurementId: 'G-B61KFJ6MVH',
-};
